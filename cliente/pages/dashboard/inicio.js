3e717d0d382adfc8a9cd23571fce22081050869c
oLva <olvapg10@gmail.com>
2022-05-28 19:52:05 +0200

    'Home' del dashboard cambiado por 'Inicio'

---
+++ cliente/pages/dashboard/inicio.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Dashboard from "../../components/Dashboard/Dashboard";
+
+const inicio = () => {
+  return (
+    <Dashboard>
+      <div>
+        <h1 className="text-[2.5rem] text-oscuro">Inicio</h1>
+      </div>
+    </Dashboard>
+  );
+};
+
+export default inicio;
